69a07401ac0ba860a89dbbb8b75b026bb8ec69c3
very verona <105114652+virtualvery@users.noreply.github.com>
2022-10-11 01:47:33 -0300

    striteri

--- ft_striteri.c
+++ ft_striteri.c
@@ -1,2 +1,13 @@
 #include "libft.h"
 
+void    ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+    unsigned int    i;
+
+    i = 0;
+    while (i < ft_strlen(s))
+    {
+        f(i, &s[i]);
+        i++;
+    }
+}
