8871b9d88942cd169409473339d0ed216c5dd54c
rverona- <rverona-@student.42sp.org.br>
2022-11-27 02:04:53 +0100

    test check

--- get_next_line.c
+++ get_next_line.c
@@ -1,76 +1,105 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rverona- <rverona-@student.42.org.br>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/24 23:08:36 by rverona-          #+#    #+#             */
+/*   Updated: 2022/11/27 02:02:25 by rverona-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
 char	*get_line(int fd, char *stash)
 {
 	char	*buffer;
-	int	i;
-	
-	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	char	*temp;
+	int		i;
+
+	buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!buffer)
 		return (NULL);
 	i = 1;
-	while (i > 0 && !ft_strchr(stash, '\0'))
+	while (i > 0 && !ft_strchr(stash, '\n'))
 	{
 		i = read(fd, buffer, BUFFER_SIZE);
 		if (i <= 0)
-		{
-			free(buffer);
-			return (NULL);
-		}
+			break ;
 		buffer[i] = '\0';
+		temp = stash;
 		stash = ft_strjoin(stash, buffer);
+		if (temp)
+			free (temp);
 	}
 	free (buffer);
-	if (i == -1)
-		return (NULL);
 	return (stash);
 }
 
-char	*get_current_line(int static_i, char *stash)
+char	*get_current_line(char *stash)
 {
 	char	*temp;
-	int	i;
-	int	aux;
-	
+	int		i;
+
 	i = 0;
-	if (!stash[static_i])
-		return (NULL);
-	aux = static_i;
-	while (stash[aux] != '\n' && stash[aux])
+	while (stash[i] != '\n' && stash[i])
 		i++;
-	temp = (char *)malloc(sizeof(char) * (i + 2));
+	temp = malloc(sizeof(char) * i + 2);
 	if (!temp)
 		return (NULL);
 	i = 0;
-	while (stash[static_i] != '\n' && stash[static_i])
+	while (stash[i] != '\n' && stash[i])
 	{
-		temp[i] = stash[static_i];
+		temp[i] = stash[i];
 		i++;
-		static_i++;
 	}
-	if (stash[static_i] == '\n')
+	if (stash[i] == '\n')
 	{
-		temp[i] = '\n';
-		static_i++;
+		temp[i] = stash[i];
 		i++;
 	}
 	temp[i] = '\0';
 	return (temp);
 }
 
-char	*get_next_line(int fd)
+char	*extra_letters(char *stash)
 {
-	static int	i;
-	char	*line;
-	static char	*stash;
+	int		i;
+	int		j;
+	char	*str;
 
 	i = 0;
+	while (stash[i] && stash[i] != '\n')
+		i++;
+	if ((stash[i] == '\n' && stash[i + 1] == '\0') || !stash[i])
+	{
+		free (stash);
+		return (NULL);
+	}
+	str = malloc(sizeof(char) * (ft_strlen(stash) - i + 1));
+	i++;
+	if (!str)
+		return (NULL);
+	j = 0;
+	while (stash[i])
+		str[j++] = stash[i++];
+	str[j] = '\0';
+	free (stash);
+	return (str);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*line;
+	static char	*stash;
+
 	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (0);
+		return (NULL);
 	stash = get_line(fd, stash);
 	if (!stash)
 		return (NULL);
-	line = get_current_line(i, stash);
+	line = get_current_line(stash);
+	stash = extra_letters(stash);
 	return (line);
-
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,28 +1,39 @@
-#include <stddef.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rverona- <rverona-@student.42.org.br>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/24 21:57:00 by rverona-          #+#    #+#             */
+/*   Updated: 2022/11/26 23:58:37 by rverona-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char *s1, char *s2)
 {
 	char	*res;
-	int	i;
+	int		i;
 
-	res = (char *)malloc(((ft_strlen(s1) + ft_strlen(s2)) + 1) * sizeof(char));
+	res = malloc(ft_strlen(s1) + ft_strlen(s2) + 1 * sizeof(char));
+	if (!res)
+		return (NULL);
 	i = 0;
-	while (*s1)
+	while (s1 != NULL && *s1)
 	{
 		res[i] = *s1;
 		s1++;
 		i++;
 	}
-	while (*s2)
+	while (s2 != NULL && *s2)
 	{
 		res[i] = *s2;
 		s2++;
 		i++;
 	}
 	res[i] = '\0';
-	if (!res)
-		return (NULL);
 	return (res);
 }
 
@@ -30,6 +41,8 @@ size_t	ft_strlen(const char *s)
 {
 	size_t	i;
 
+	if (!s)
+		return (0);
 	i = 0;
 	while (s[i] != '\0')
 		i++;
@@ -43,7 +56,7 @@ char	*ft_strchr(char *s, int c)
 	i = ft_strlen(s);
 	while (i >= 0)
 	{
-		if (s[i] == (char)c)
+		if (s != NULL && s[i] == (char)c)
 			return (&((char *)s)[i]);
 		i--;
 	}
